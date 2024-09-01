251043e359f1d370330c7f8d6b73f72787c7f402
Elran Shefer <elran777@gmail.com>
2024-09-01 18:41:01 +0300

    fixup: delete redundent code

--- tfstate_git/server/repository.py
+++ tfstate_git/server/repository.py
@@ -1,51 +1,15 @@
-import abc
 from contextlib import suppress
 import json
 import os
 import pathlib
 import subprocess
-from pydantic import BaseModel
 
+from tfstate_git.server.base_state_lock_provider import BaseStateLockProvider, LockBody, LockingError
 from tfstate_git.utils.dependency_downloader import DependenciesManager
 from tfstate_git.utils.sops_controller import Sops
 
 
-class LockBody(BaseModel):
-    model_config = {
-        "from_attributes": True,
-    }
-
-    ID: str
-    Operation: str
-    Who: str
-    Version: str
-    Created: str
-
-
-class LockingError(Exception):
-    def __init__(self, msg: str, lock_id: str) -> None:
-        super().__init__(msg)
-        self.lock_id = lock_id
-
-
-class BaseStateLockRepository(abc.ABC):
-    @abc.abstractmethod
-    async def get(self): ...
-
-    @abc.abstractmethod
-    async def put(self, lock_id: str, value: dict): ...
-
-    @abc.abstractmethod
-    async def delete(self, lock_id: str): ...
-
-    @abc.abstractmethod
-    def lock(self, lock_id: str, data: LockBody): ...
-
-    @abc.abstractmethod
-    def unlock(self): ...
-
-
-class GitStateLockRepository(BaseStateLockRepository):
+class GitStateLockRepository(BaseStateLockProvider):
     def __init__(
         self,
         manager: DependenciesManager,
